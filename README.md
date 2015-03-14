4848c7bb03bfd37f30a30f5c4a73e433842d179f
luismoramedina <luismoramedina@gmail.com>
2015-03-14 17:45:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Description
+
+Login web with a get functional/integration test with the  Firefox driver.
+
+# Usage
+
+cd src/main/webapp
+python -m http.server
+
+cd ../..
+mvn test
---
+++ src/test/groovy/GebishOrgSpec.groovy
@@ -0,0 +1,24 @@
+import geb.spock.GebReportingSpec
+
+class GebishOrgSpec extends GebReportingSpec {
+
+    def "go to login"() {
+        when:
+        go "http://localhost:8000/index.html"
+
+        then:
+        title == "Index"
+    }
+
+    def "login"() {
+        given:
+        go "http://localhost:8000/index.html"
+
+        when:
+        username = "admin"
+        $("input[type=submit]").click()
+
+        then:
+        title == "Server"
+    }
+}
